8c1ed4d42a11afa3d85235acabe6d4bd9c6fd1cb
jichenghu <jicheng@yahoo.com>
2021-12-28 10:16:55 +0800

    Add project files.

---
+++ uwpLib/AutoSuggestEditBox.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace uwpLib
+{
+    public sealed partial class AutoSuggestEditBox : UserControl
+    {
+        /// <summary>
+        /// type propdp and then press tab tab
+        /// </summary>
+        public int MyProperty
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(AutoSuggestEditBox), null);
+
+        public string XamlIslandMessage { get; set; }
+
+        public AutoSuggestEditBox()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Popup p = this.Parent as Popup;
+            if (p != null)
+            {
+                p.IsOpen = false;
+            }
+        }
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                List<string> suggestions = new List<string>()
+                {
+                    sender.Text + "1",
+                    sender.Text + "2"
+                };
+                Control1.ItemsSource = suggestions;
+            }
+        }
+
+        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            //SuggestionOutput.Text = args.SelectedItem.ToString();
+            XamlIslandMessage = args.SelectedItem.ToString();
+        }
+    }
+}
